936034625c4f5775c5cbef68ec0f1c643116bb27
Joseph P Cohen <joseph.cohen001@gibbshead.(none)>
2016-01-16 14:15:53 -0500

    ok

--- CratersPython/runFolds.sh
+++ CratersPython/runFolds.sh
@@ -1,8 +1,8 @@
 
 
-for i in {1..10}; do python train_craters2.py --fold $i --region East; done 2>&1 | tee logEast
-for i in {1..10}; do python train_craters2.py --fold $i --region Center; done 2>&1 | tee logCenter
-for i in {1..10}; do python train_craters2.py --fold $i --region West; done 2>&1 | tee logWest
+for i in {1..10}; do python train_craters.py --fold $i --region East --gpus 0,1,2,3,4,5,6,7; done 2>&1 | tee logEast
+for i in {1..10}; do python train_craters.py --fold $i --region Center --gpus 0,1,2,3,4,5,6,7; done 2>&1 | tee logCenter
+for i in {1..10}; do python train_craters.py --fold $i --region West --gpus 0,1,2,3,4,5,6,7; done 2>&1 | tee logWest
 
-cat logEast | grep "Epoch\[49\] Val"
-cat logEast | grep "Epoch\[49\] Val" | awk '{ print $2}' | awk  -F= '{ total += $2; count++ } END { print total/count }'
+cat logEast | grep "Epoch\[99\] Val"
+cat logEast | grep "Epoch\[99\] Val" | awk '{ print $5}' | awk  -F= '{ total += $2; count++ } END { print total/count }'
